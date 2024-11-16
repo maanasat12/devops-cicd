7672a2a63ed7e6ac685bbb75e5300c3a7fba0ff0
Maanasa Tiruveedhula <mn233351@dal.ca>
2024-11-16 14:05:01 -0400

    New bucket name

--- dev/gcsUpload.py
+++ dev/gcsUpload.py
@@ -1,5 +1,5 @@
 from gcloud import storage
 storage_client = storage.Client()
-bucket = storage_client.get_bucket("mt-devcicdbucket")
+bucket = storage_client.get_bucket("maanasat_bucket7")
 blob = bucket.blob('tfolder/newtxt.txt')  #file to be uploaded
 blob.upload_from_filename('./test.txt')  #local fil
